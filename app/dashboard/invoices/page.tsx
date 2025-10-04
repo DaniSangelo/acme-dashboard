326257a026b8cbeb6943dfdf322cc0d54fc72fcc
Daniel Sângelo <danielsangelo@hotmail.com>
2025-10-03 21:59:32 -0300

    feat: implement invoice searching

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,38 @@
-const InvoicesPage = () => {
+import { lusitana } from "@/app/ui/fonts";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Pagination from "@/app/ui/invoices/pagination";
+import InvoicesTable from "@/app/ui/invoices/table";
+import Search from "@/app/ui/search";
+import { Suspense } from "react";
+
+interface InvoicePageProps {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>
+}
+
+const InvoicesPage = async ({ searchParams }: InvoicePageProps) => {
+    const query = (await searchParams)?.query || '';
+    const currentPage = Number((await searchParams)?.page) || 1;
+
     return (
-        <div>
-            <h1>Invoices</h1>
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2x1`}>
+                    Invoices
+                </h1>
+            </div>
+            <div className="mt-3 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search inovices..." />
+                <CreateInvoice/>
+            </div>
+            <Suspense>
+                <InvoicesTable query={query} currentPage={currentPage} />
+            </Suspense>
+            <div className="mt-5 flex w-full justify-center">
+                {/* <Pagination totalPages={totalPages} */}
+            </div>
         </div>
     );
 }
