4ab069fd66ff06566dad646b3b69531d23240815
Daniel Sângelo <danielsangelo@hotmail.com>
2025-10-05 10:09:32 -0300

    feat: handling with errors gracefully

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -22,10 +22,14 @@ export async function createInvoices(formData: FormData) {
     const validatedData = CreateInvoice.parse({ customerId, amount, status })
     const amountInCents = validatedData.amount * 100
     const date = new Date().toISOString().split('T')[0];
-
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId as string}, ${amountInCents}, ${status as string}, ${date})`;
+    try {
+      await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId as string}, ${amountInCents}, ${status as string}, ${date})`;
+    } catch (error) {
+      console.log(`Something went wrong: ${error}`);
+      return { message: 'Failed to create invoice'}
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
@@ -41,12 +45,17 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
- 
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    console.log(`Something went wrong: ${error}`);
+    return { message: 'Failed to update invoice' };
+  }
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
